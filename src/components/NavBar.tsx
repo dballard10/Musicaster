d08ca5a97ffb5cc77952b5de1330e7f7e7679617
dballard10 <dylanballard55@gmail.com>
2025-01-27 19:56:01 -0500

    Search UI

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,9 +2,13 @@ import { HStack, Text, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import logo from "../assets/Logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
-//import SearchInput from "./SearchInput";
+import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Link to="/">
@@ -15,7 +19,7 @@ const NavBar = () => {
           </Text>
         </HStack>
       </Link>
-      {/* <SearchInput /> */}
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,34 @@
+import { Input } from "@chakra-ui/react";
+import { InputGroup } from "../components/ui/input-group";
+import { BsSearch } from "react-icons/bs";
+import { useRef } from "react";
+
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) {
+          onSearch(ref.current.value);
+        }
+      }}
+    >
+      <InputGroup startElement={<BsSearch />}>
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="outline"
+        />
+      </InputGroup>
+    </form>
+  );
+};
+
+export default SearchInput;
