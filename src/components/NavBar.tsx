613b57d664accf17c8f3b317f1bac100296e22da
dballard10 <dylanballard55@gmail.com>
2025-01-27 19:45:31 -0500

    Add musicaster logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
-import { HStack, Text } from "@chakra-ui/react";
+import { HStack, Text, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-//import logo from "../assets/logo.webp";
+import logo from "../assets/Logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 //import SearchInput from "./SearchInput";
 
@@ -8,10 +8,12 @@ const NavBar = () => {
   return (
     <HStack padding="10px">
       <Link to="/">
-        {/* <Image src={logo} boxSize="60px" objectFit="cover" /> */}
-        <Text fontSize="2xl" fontWeight="bold">
-          Musicaster
-        </Text>
+        <HStack>
+          <Image src={logo} boxSize="60px" objectFit="cover" />
+          <Text fontSize="2xl" fontWeight="bold">
+            Musicaster
+          </Text>
+        </HStack>
       </Link>
       {/* <SearchInput /> */}
       <ColorModeSwitch />
